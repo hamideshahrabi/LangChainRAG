65256efaa46f2b0ee43a28ded136f2c9ccb3a4aa
Hami Shah <39047108+hamideshahrabi@users.noreply.github.com>
2025-11-24 17:15:12 -0500

    Re-add imports and setup for Langchain demo

--- chatbot/localama.py
+++ chatbot/localama.py
@@ -1,3 +1,4 @@
+# 2
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
@@ -30,4 +31,5 @@ output_parser=StrOutputParser()
 chain=prompt|llm|output_parser
 
 if input_text:
+
     st.write(chain.invoke({"question":input_text}))
